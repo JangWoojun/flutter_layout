1588cd6d045d420bf7c420e33582c9463d2b3be7
JangWoojun <woojun0107@naver.com>
2024-01-22 08:23:19 +0900

    Widget 상하좌우 배치

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("Widget 좌우 배치"),),
+      appBar: AppBar(title: Text("Widget 상하좌우 배치"),),
       body: const Body(),
     ),
   ));
@@ -14,43 +14,50 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-            child: Text(
-              "Container 1",
-              style: TextStyle(color: Colors.white),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text(
+                "Container 1",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-            child: Text(
-              "Container 2",
-              style: TextStyle(color: Colors.white),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              child: Text(
+                "Container 2",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-            child: Text(
-              "Container 3",
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
-      ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Text(
+                "Container 3",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+        Container(
+          width: 300,
+          height: 120,
+          color: Colors.grey,
+          child: Text("Container 4", style: TextStyle(color: Colors.white),),
+        )
+      ]
     );
   }
 }
