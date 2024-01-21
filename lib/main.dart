4c99c0075077ced9b28226e209bdb01955a02b76
JangWoojun <woojun0107@naver.com>
2024-01-22 08:30:58 +0900

    Widget 상하 스크롤

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("Widget 상하좌우 배치"),),
+      appBar: AppBar(title: Text("상하 스크롤"),),
       body: const Body(),
     ),
   ));
@@ -14,50 +14,73 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          mainAxisSize: MainAxisSize.max,
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
           children: [
             Container(
               width: 100,
               height: 100,
               color: Colors.red,
-              child: Text(
-                "Container 1",
-                style: TextStyle(color: Colors.white),
-              ),
             ),
             Container(
               width: 100,
               height: 100,
               color: Colors.green,
-              child: Text(
-                "Container 2",
-                style: TextStyle(color: Colors.white),
-              ),
             ),
             Container(
               width: 100,
               height: 100,
               color: Colors.blue,
-              child: Text(
-                "Container 3",
-                style: TextStyle(color: Colors.white),
-              ),
-            )
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.grey,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.grey,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.grey,
+            ),
           ],
         ),
-        Container(
-          width: 300,
-          height: 120,
-          color: Colors.grey,
-          child: Text("Container 4", style: TextStyle(color: Colors.white),),
-        )
-      ]
+      ),
     );
   }
 }
