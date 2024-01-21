8295483cd6a54940bbd3bde362b21b3725c229b1
JangWoojun <woojun0107@naver.com>
2024-01-22 08:47:35 +0900

    Widget 비율로 배치

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("좌우 스크롤"),),
+      appBar: AppBar(title: Text("Widget 비율로 배치"),),
       body: const Body(),
     ),
   ));
@@ -14,72 +14,12 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.green,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.grey,
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        Container(color: Colors.blue, height: 200,),
+        Expanded(child: Container(color: Colors.red, height: 100,)), // 자식 크기 상관 없이 꽉채움
+        Flexible(child: Container(color: Colors.blue, height: 100,)) // 자식 크기에 따라 채워지나 없다면 꽉 채움
+      ],
     );
   }
 }
