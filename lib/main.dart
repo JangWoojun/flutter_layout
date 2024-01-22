e360bccfb14ac3c03aca9e5c53604a6cea8320a7
JangWoojun <woojun0107@naver.com>
2024-01-22 09:17:42 +0900

    stack과 Align을 이용한 배지 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -16,40 +16,31 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: 500,
-          height: 400,
-          color: Colors.black,
-        ),
-        Container(
-          width: 400,
-          height: 300,
-          color: Colors.red,
-        ),
-        Container(
+    return Stack(children: [
+      Align(
+        alignment: Alignment.center,
+        child: Container(
           width: 300,
-          height: 200,
-          color: Colors.green,
+          height: 300,
+          decoration: BoxDecoration(
+              color: Colors.blue, borderRadius: BorderRadius.circular(400)),
         ),
-        Align(
-          alignment: const Alignment(1, 0),
-          child: Container(
-            width: 200,
-            height: 100,
-            color: Colors.blue,
+      ),
+      Align(
+        alignment: Alignment.center,
+        child: Container(
+          width: 280,
+          height: 280,
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(400)),
+          child: Center(
+            child: Text(
+              "count 0",
+              style: TextStyle(color: Colors.blue, fontSize: 30),
+            ),
           ),
         ),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 100,
-            height: 50,
-            color: Colors.yellow,
-          ),
-        ),
-      ],
-    );
+      ),
+    ]);
   }
 }
